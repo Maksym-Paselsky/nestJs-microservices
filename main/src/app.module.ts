780ae938d59fcdbbb81d0b5ce5f156f83731914a
KOMA TOZ <43058251+Maksym-Paselsky@users.noreply.github.com>
2023-05-25 12:06:18 +0300

    coonect mongo

--- main/src/app.module.ts
+++ main/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ProductModule } from './product/product.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_DB || ''),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
